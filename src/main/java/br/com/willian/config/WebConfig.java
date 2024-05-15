d27e02de4859e692202dc923341d2919a968f784
Willian Costa <willian95@outlook.com>
2024-05-15 13:40:11 -0300

    feat: add content negotiation for header

--- src/main/java/br/com/willian/config/WebConfig.java
+++ src/main/java/br/com/willian/config/WebConfig.java
@@ -8,10 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+   /* @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
                 .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }*/
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false).ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
     }
