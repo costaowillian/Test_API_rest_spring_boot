432564c7728c61e3eae521401714170de2e9703a
Willian Costa <willian95@outlook.com>
2024-05-16 14:25:20 -0300

    feat: add book repository

---
+++ src/main/java/br/com/willian/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package br.com.willian.repositories;
+
+import br.com.willian.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
