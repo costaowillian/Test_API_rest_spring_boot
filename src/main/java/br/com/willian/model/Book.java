d042918ac84538db3d04006b3445192525c4ade1
Willian Costa <willian95@outlook.com>
2024-05-16 14:23:37 -0300

    feat: add entity book

---
+++ src/main/java/br/com/willian/model/Book.java
@@ -0,0 +1,99 @@
+package br.com.willian.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "books")
+public class Book implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name =  "author", nullable = false, length = 200)
+    private String author;
+
+    @Column(name =  "launch_date", nullable = false)
+    private Date launch_date;
+
+    @Column(name =  " price", nullable = false)
+    private Double price;
+
+    @Column(name =  "title", nullable = false, length = 400)
+    private String title;
+
+    public Book(){}
+
+    public Book(Long id, String author, Date launch_date, Double price, String title) {
+        this.id = id;
+        this.author = author;
+        this.launch_date = launch_date;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Book(String author, Date launch_date, Double price, String title) {
+        this.author = author;
+        this.launch_date = launch_date;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunch_date() {
+        return launch_date;
+    }
+
+    public void setLaunch_date(Date launch_date) {
+        this.launch_date = launch_date;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(author, book.author) && Objects.equals(launch_date, book.launch_date) && Objects.equals(price, book.price) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, launch_date, price, title);
+    }
+}
