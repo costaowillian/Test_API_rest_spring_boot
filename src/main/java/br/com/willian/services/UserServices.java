a8b10b5d151b3e480084a9ec79b1c983de709314
Willian Costa <willian95@outlook.com>
2024-05-20 11:04:43 -0300

    feat: add UserServices

---
+++ src/main/java/br/com/willian/services/UserServices.java
@@ -0,0 +1,48 @@
+package br.com.willian.services;
+
+import br.com.willian.controllers.PersonController;
+import br.com.willian.dtos.PersonDTO;
+import br.com.willian.exceptions.DuplicateResourceException;
+import br.com.willian.exceptions.RequiredObjectIsNullException;
+import br.com.willian.exceptions.ResourceNotFoundException;
+import br.com.willian.model.Person;
+import br.com.willian.model.User;
+import br.com.willian.repositories.PersonRepository;
+import br.com.willian.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Service
+public class UserServices implements UserDetailsService {
+
+	private Logger logger = Logger.getLogger(UserServices.class.getName());
+	
+	@Autowired
+	private UserRepository repository;
+
+	public UserServices(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		logger.info("Finding one user by name...");
+		User user = repository.findByUserName(username);
+		if(user != null) {
+			return  user;
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+	}
+}
