db2009b4d5c7213d758e893d5badeac8379dc9c6
Willian Costa <willian95@outlook.com>
2024-05-22 13:45:05 -0300

    feat: implement mutiple upload on file controller

--- src/main/java/br/com/willian/controllers/FileController.java
+++ src/main/java/br/com/willian/controllers/FileController.java
@@ -18,7 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Tag(name = "Files", description = "Endpoints For Managing files")
 @RestController
@@ -57,5 +60,31 @@ public class FileController {
 
         return new UploadFileResponseDTO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
     }
+
+    @PostMapping("/uploadMultipleFile")
+    @Operation(
+            summary = "Upload Multiple File",
+            description = "Upload Multiple File",
+            tags = {"Files"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON,
+                                            array = @ArraySchema(schema = @Schema(implementation = UploadFileResponseDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
+    public List<UploadFileResponseDTO> uploadMultipleFile(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing files to disk...");
+        return Arrays.stream(files).map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+
 
 }
