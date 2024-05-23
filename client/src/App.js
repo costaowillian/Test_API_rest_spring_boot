4e66cd6b225ec38bb9b71d903658814b23ff0b6a
Willian Costa <willian95@outlook.com>
2024-05-23 13:50:42 -0300

    chore: add client application

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './pages/login';
+import './global.css'
+
+export default function App() {
+  return (
+   <Login />
+  );
+}
+
+
---
+++ client/src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css';
+
+export default function Login() {
+    return (
+        <>
+            <h1>
+                Bem vindo!
+            </h1>
+        </>
+    )
+}
