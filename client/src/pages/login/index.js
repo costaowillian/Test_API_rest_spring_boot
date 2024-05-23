63955add2bc3c6879116ef501661002ed0357ca1
Willian Costa <willian95@outlook.com>
2024-05-23 14:20:18 -0300

    feat: add page login

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import './styles.css';
+import logoimage from '../../assets/livro.png';
+import imgBanner from '../../assets/audio-livro.png'
 
 export default function Login() {
     return (
         <>
-            <h1>
-                Bem vindo!
-            </h1>
+            <div className="login-container">
+                <section className="form">
+                    <img src={ logoimage } alt="logo"></img>
+                    <form>
+                        <h1>
+                            Acces your Account
+                        </h1>
+                        <input type="text" placeholder="UserName" />
+                        <input type="password" placeholder="Password" />
+                        <button className="button" type="submit">Login</button>
+                    </form>
+                </section>
+                <img src={ imgBanner } alt="login"></img>
+            </div>
         </>
     )
 }
