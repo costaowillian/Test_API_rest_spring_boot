78e1382ea422c357e2c11902f67be745f05d918e
Willian Costa <willian95@outlook.com>
2024-05-24 10:18:02 -0300

    feat: add logout

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -3,12 +3,21 @@ import { Link } from "react-router-dom";
 import './styles.css';
 import logoImg from '../../assets/livro.png';
 import { FiPower } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 export default function Header(props) {
+
+    const navigate = useNavigate([]); 
 
     const { isNewBook } = props;
 
     const userName = localStorage.getItem('userName');
+
+    const logout = () => {
+        const accessToken = localStorage.getItem('accessToken');
+        localStorage.clear();
+        navigate('/');
+    }
 
     return(
         <> 
@@ -16,7 +25,7 @@ export default function Header(props) {
             <Link to="/books" ><img src={ logoImg } alt="logo"></img></Link>
             <span> Welcome, <strong>{userName}</strong>!</span>
             {!isNewBook && <Link className="button" to="/book/new">Add new Book</Link>}
-            <button type="button">
+            <button type="button" onClick={logout}>
                 <FiPower size={18} color="#251fc5" />
             </button> 
         </header>
