90c5368eef8e309466a90a18e210f2e26b4ba0f3
Willian Costa <willian95@outlook.com>
2024-05-24 10:14:04 -0300

    feat: add request to get all books

--- client/src/pages/books/index.js
+++ client/src/pages/books/index.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './styles.css';
 import Header from "../../components/header/index";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import api from '../../services/api';
 
 export default function Book() {
+    
+    const navigate = useNavigate([]); 
+    
+    const [books, setBooks] = useState();
+
+    const accessToken = localStorage.getItem('accessToken');
+    
+    useEffect(() => {
+        api.get("/api/v1/books", {
+            headers: {
+                'authorization': `Bearer ${accessToken}`
+            }, 
+            params: {
+                'page': 0,
+                'size': 4,
+                'direction': 'asc'
+            }
+        }).then(response => {
+            setBooks(response.data._embedded.booksDTOList);
+        })
+    }, []);
+
+    const deleteBook = async(id) => {
+        try {
+           await api.delete(`/api/v1/books/${id}`, {headers: {
+            'authorization': `Bearer ${accessToken}`
+        }});
+
+        setBooks(books.filter(book => book.id !== id));
+        }catch (err) {
+            alert('Delete failed" Try again!');
+        }
+    }
+
     return(
         <>
             <div className="book-container"> 
@@ -12,57 +48,29 @@ export default function Book() {
                 <h1>Registered Books</h1>
 
                 <ul>
-                    <li>
-                        <strong>Tittle:</strong>
-                        <p>Dooker Deep Dive</p>
-                        <strong>Author</strong>
-                        <p>Nigel Poulton</p>
-                        <strong>Price:</strong>
-                        <p>R$ 47,00</p>
-                        <strong>Release Date:</strong>
-                        <p>12/07/2017</p>
+                    {books ? (
+                        books.map(book => (
+                            <li key={book.id}>
+                                <strong>Title:</strong>
+                                <p>{book.title}</p>
+                                <strong>Author:</strong>
+                                <p>{book.author}</p>
+                                <strong>Price:</strong>
+                                <p>{Intl.NumberFormat('pt-br', {style:'currency', currency: 'brl'}).format(book.price)}</p>
+                                <strong>Release Date:</strong>
+                                <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.release_date))}</p>
 
-                        <button type="button">
-                            <FiEdit size={20} color="#251fc5" />
-                        </button>
-                        <button type="button">
-                            <FiTrash2 size={20} color="#251fc5" />
-                        </button>
-                    </li>
-                    <li>
-                        <strong>Tittle:</strong>
-                        <p>Dooker Deep Dive</p>
-                        <strong>Author</strong>
-                        <p>Nigel Poulton</p>
-                        <strong>Price:</strong>
-                        <p>R$ 47,00</p>
-                        <strong>Release Date:</strong>
-                        <p>12/07/2017</p>
-
-                        <button type="button">
-                            <FiEdit size={20} color="#251fc5" />
-                        </button>
-                        <button type="button">
-                            <FiTrash2 size={20} color="#251fc5" />
-                        </button>
-                    </li>
-                    <li>
-                        <strong>Tittle:</strong>
-                        <p>Dooker Deep Dive</p>
-                        <strong>Author</strong>
-                        <p>Nigel Poulton</p>
-                        <strong>Price:</strong>
-                        <p>R$ 47,00</p>
-                        <strong>Release Date:</strong>
-                        <p>12/07/2017</p>
-
-                        <button type="button">
-                            <FiEdit size={20} color="#251fc5" />
-                        </button>
-                        <button type="button">
-                            <FiTrash2 size={20} color="#251fc5" />
-                        </button>
-                    </li>
+                                <button type="button" onClick={() => deleteBook(book.id)}>
+                                    <FiEdit size={20} color="#251fc5" />
+                                </button>
+                                <button type="button">
+                                    <FiTrash2 size={20} color="#251fc5" />
+                                </button>
+                            </li>))) 
+                            : (
+                            <li>No books available</li>
+                        )
+                    }
                 </ul>
             </div>
         </>
