8ec1fc7a5e4eadf7777353bad9fa8d589c88e8d1
Willian Costa <willian95@outlook.com>
2024-05-24 09:28:55 -0300

    feat: implements request login whit axios

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import './styles.css';
 import logoimage from '../../assets/livro.png';
 import imgBanner from '../../assets/audio-livro.png'
 
+import api from '../../services/api';
+
 export default function Login() {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const login = async(e) => {
+        e.preventDefault();
+        let data = JSON.stringify({
+            "userName": userName,
+            "password": password
+          });
+
+        try {
+            const response = await api.post('/auth/signin', data);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            console.log(response);
+            navigate('/books');
+        } catch (err) {
+            console.log(err);
+            alert("Login failed! Try again!")
+        }
+    }
+
     return (
         <>
             <div className="login-container">
                 <section className="form">
                     <img src={ logoimage } alt="logo"></img>
-                    <form>
+                    <form onSubmit={login}>
                         <h1>
                             Acces your Account
                         </h1>
-                        <input type="text" placeholder="UserName" />
-                        <input type="password" placeholder="Password" />
+                        <input type="text" placeholder="UserName" value={userName} onChange={e => setUserName(e.target.value)} />
+                        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
                         <button className="button" type="submit">Login</button>
                     </form>
                 </section>
